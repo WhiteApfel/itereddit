865d113488824c3186eec40b437be11d73ca66f0
Eugene Mayer <white@pfel.ru>
2021-10-14 11:45:50 +0300

    ✨ Add HTTP client

--- itereddit/client.py
+++ itereddit/client.py
@@ -1,3 +1,6 @@
+from httpx import AsyncClient
+
+
 class Itereddit:
     """
     A handy tool for parsing posts in subreddit
@@ -7,3 +10,10 @@ class Itereddit:
     """
     def __init__(self, subreddit: str):
         self.subreddit = subreddit
+        self._client = None
+
+    @property
+    def client(self):
+        if not self._client:
+            self._client = AsyncClient()
+        return self._client
