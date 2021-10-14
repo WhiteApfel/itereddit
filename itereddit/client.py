667ed73aa69642f26cf02c6cc50dbd7ee56d7dd3
Eugene Mayer <white@pfel.ru>
2021-10-14 11:39:21 +0300

    🎉 Create project

---
+++ itereddit/client.py
@@ -0,0 +1,9 @@
+class Itereddit:
+    """
+    A handy tool for parsing posts in subreddit
+
+    :param subreddit: name of subreddit
+    :type subreddit: str
+    """
+    def __init__(self, subreddit: str):
+        self.subreddit = subreddit
