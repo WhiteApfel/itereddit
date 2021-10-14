cae56af99b80db87f07098dca9297ad86e8b24d2
Eugene Mayer <white@pfel.ru>
2021-10-14 13:21:21 +0300

    🏷️ Reorganize dataclasses to support more media-types

--- itereddit/models/subreddit_posts.py
+++ itereddit/models/subreddit_posts.py
@@ -2,27 +2,36 @@ from __future__ import annotations
 
 from typing import Any, Dict, List, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, root_validator
 
 
-class SubredditThumbnail(BaseModel):
-    url: str
-    width: int
-    height: int
+class SubredditMediaResolution(BaseModel):
+    url: Optional[str]
+    width: Optional[int]
+    height: Optional[int]
+    u: Optional[str]
+    x: Optional[int]
+    y: Optional[int]
 
-
-class SubredditPostOneMedia(BaseModel):
-    y: int
-    x: int
-    url: str = Field(..., alias="u")
+    @root_validator(pre=True)
+    def compatibility(cls, values):
+        comp = {
+            "u": "url",
+            "x": "width",
+            "y": "height"
+        }
+        for k in comp:
+            if k in values:
+                values[comp[k]] = values.get(k)
+        return values
 
 
 class SubredditPostMediaMetadata(BaseModel):
     status: str
     e: str
     mime_type: str = Field(..., alias="m")
-    original: List[SubredditPostOneMedia] = Field(..., alias='s')
-    preview: List[SubredditPostOneMedia] = Field(..., alias='p')
+    original: SubredditMediaResolution = Field(..., alias='s')
+    preview: List[SubredditMediaResolution] = Field(..., alias='p')
 
 
 class SubredditPostMediaMetadatas(BaseModel):
@@ -30,9 +39,13 @@ class SubredditPostMediaMetadatas(BaseModel):
 
 
 class SubredditPostMedia(BaseModel):
-    obfuscated: Any
+    obfuscated: Optional[str]
+    content: Optional[str]
     type: str
-    media_metadata: SubredditPostMediaMetadatas = Field(..., alias="mediaMetadata")
+    media_metadata: Optional[SubredditPostMediaMetadatas] = Field(None, alias="mediaMetadata")
+    resolutions: Optional[List[SubredditMediaResolution]]
+    gallery: Optional[dict]
+    source: Optional[dict]
 
 
 class SubredditPost(BaseModel):
@@ -40,16 +53,15 @@ class SubredditPost(BaseModel):
     num_comments: int = Field(..., alias="numComments")
     created: int
     score: int
-    thumbnail: SubredditThumbnail
+    thumbnail: Optional[SubredditMediaResolution]
     title: str
     author: str
     author_id: str = Field(..., alias="authorId")
     post_id: str = Field(..., alias="postId")
     upvote_ratio: float = Field(..., alias="upvoteRatio")
     permalink: str
-    media: SubredditPostMedia
-    gallery: dict
-    source: dict
+    media: Optional[SubredditPostMedia]
+    preview: Optional[SubredditMediaResolution]
 
 
 class SubredditPosts(BaseModel):
